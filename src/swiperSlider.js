7d96b92b20c79af5d1bd9082c86cc9d23951576c
Wojciech <wojciechonoszko@gmail.com>
2022-12-14 12:52:12 +0100

    update

---
+++ src/swiperSlider.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 3,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
