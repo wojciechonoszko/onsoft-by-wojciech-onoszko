2c351cbb4f28b6e20c4abe741a3006c7692ab4fc
Wojciech <wojciechonoszko@gmail.com>
2022-12-21 20:52:06 +0100

    updating

--- src/stickyScript.js
+++ src/stickyScript.js
@@ -5,6 +5,7 @@ const header = document.getElementById('header-menu');
 const headerContainer = document.getElementById('header-menu');
 let navbarMargin = document.getElementById('about-me');
 let portfolioMargin = document.getElementById('section-portfolio');
+let footerMargin = document.getElementById('footer');
 
 // Get the offset position of the navbar
 const sticky = header.offsetTop;
@@ -16,11 +17,13 @@ function myFunction() {
     headerContainer.classList.add('sticky-mobile-container')
     navbarMargin.classList.add('navbarOffsetMargin');
     portfolioMargin.classList.add('navbarOffsetMargin');
+    footerMargin.classList.add('navbarOffsetMargin');
   } else {
     header.classList.remove('sticky');
     headerContainer.classList.remove('sticky-mobile-container');
     navbarMargin.classList.remove('navbarOffsetMargin');
     portfolioMargin.classList.remove('navbarOffsetMargin');
+    footerMargin.classList.remove('navbarOffsetMargin');
   }
 }
 
