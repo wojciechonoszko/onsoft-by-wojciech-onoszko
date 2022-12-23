1328662a35128d94504399b51e5324bf5c32fad5
wojciechonoszko <90242269+wojciechonoszko@users.noreply.github.com>
2022-12-23 08:55:00 +0000

    Deploying to gh-pages from @ wojciechonoszko/onsoft-by-wojciech-onoszko@81e8782e85ac51ef8985a72ee853b7fa05733100 🚀

--- src.629387d4.js
+++ src.629387d4.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/world.jpg":[["world.aac902a4.jpg","xtLK"],"xtLK"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/onsoft-by-wojciech-onoszko/src.2066c11b.js.map
+//# sourceMappingURL=/onsoft-by-wojciech-onoszko/src.629387d4.js.map
